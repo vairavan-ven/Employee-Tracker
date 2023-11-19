ef652b78e7bb621026d0e7bdc761b35c62910044
vairavan-ven <vairavan.ven@gmail.com>
2023-11-19 19:22:33 +1100

    made changes to SQL files

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,22 +6,21 @@ INSERT INTO department (name) VALUES
  
 
 INSERT INTO role (title, salary, department_id) VALUES
-  ('Sales Representative', 60000.00, 1),
-  ('HR Representative', 70000.00, 2),
-  ('Accounting Manager', 65000.00, 3),
-  ('Accountant', 55000.00, 3),
-  ('Customer Service Specialist', 50000.00, 4),
-  ('Marketing Coordinator', 60000.00, 1),
-  ('Operations Manager', 80000.00, 4);
+  ('Sales Representative', 60000, 1),
+  ('HR Representative', 70000, 2),
+  ('Accounting Manager', 65000, 3),
+  ('Accountant', 55000, 3),
+  ('Customer Service Specialist', 50000, 4),
+  ('Marketing Coordinator', 60000, 1),
+  ('Operations Manager', 80000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-  ('Jim', 'Halpert', 1, NULL),
-  ('Pam', 'Beesly', 6, 1),
-  ('Michael', 'Scott', 2, NULL),
-  ('Dwight', 'Schrute', 7, 3),
-  ('Angela', 'Martin', 4, 3),
-  ('Oscar', 'Martinez', 3, 7),
+  ('Jim', 'Halpert', 1, NULL), 
+  ('Pam', 'Beesly', 6, 1), 
+  ('Michael', 'Scott', 2, NULL), 
   ('Andy', 'Bernard', 5, 2),
-  ('Kevin', 'Malone', 4, 3),
-  ('Phyllis', 'Vance', 5, 7);
-
+  ('Dwight', 'Schrute', 7, NULL), 
+  ('Angela', 'Martin', 4, 7), 
+  ('Oscar', 'Martinez', 3, 7), 
+  ('Kevin', 'Malone', 4, 7),
+  ('Phyllis', 'Vance', 5, 7); 
